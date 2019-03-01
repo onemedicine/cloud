d06b6b8c29782a66816d8204c568c148dd17e0ec
Rémy Garrigue <remy.garrigue@gmail.com>
2019-03-01 16:24:47 +0100

    Adding hugo

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ WKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 docker network create --driver overlay apps
 docker stack deploy --compose-file ./cloudtorrent/docker-compose.yml torrent
 docker stack deploy --compose-file ./glances/docker-compose.yml monitoring
+docker stack deploy --compose-file ./hugo/docker-compose.yml blog
 docker stack deploy --compose-file ./nextcloud/docker-compose.yml drive
 docker stack deploy --compose-file ./privatebin/docker-compose.yml paste
 docker stack deploy --compose-file ./riot/docker-compose.yml matrix
