4e3e0cce5813d3793c3f29df14ac511c3aa37a7b
Rémy Garrigue <remy.garrigue@gmail.com>
2019-03-01 16:04:08 +0100

    Work in progress

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Island
 
 Just my own little cloud via Docker.
+
+## TODO
+
+- Remove placement constraint on traefik and add a dyndns for a {DOMAIN}
+- Remove placement constraint on shadowsocks and add a dyndns for a vpn.{DOMAIN}
--- start.sh
+++ start.sh
@@ -3,5 +3,11 @@
 WKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker network create --driver overlay apps
+docker stack deploy --compose-file ./cloudtorrent/docker-compose.yml torrent
+docker stack deploy --compose-file ./glances/docker-compose.yml monitoring
+docker stack deploy --compose-file ./nextcloud/docker-compose.yml drive
+docker stack deploy --compose-file ./privatebin/docker-compose.yml paste
+docker stack deploy --compose-file ./riot/docker-compose.yml matrix
+docker stack deploy --compose-file ./shadowsocks/docker-compose.yml vpn
+docker stack deploy --compose-file ./thelounge/docker-compose.yml irc
 docker stack deploy --compose-file ./traefik/docker-compose.yml rp
-docker stack deploy --compose-file ./cloudtorrent/docker-compose.yml dl
